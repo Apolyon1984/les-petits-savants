156cb85894f09dedfa79e7e22f35c6f5dd4c9878
Apolyon1984 <lnkouessom@gmail.com>
2025-06-15 02:07:27 +0000

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 import Head from "next/head";
 
 export default function Home() {
